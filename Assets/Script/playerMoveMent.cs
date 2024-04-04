3226874f9466b53c14aacd81ec6cb3b8eaefd39e
ReoMiyazaki <K021g1153@g.neec.ac.jp>
2024-04-04 19:50:29 +0900

    Initial commit

---
+++ Assets/Script/playerMoveMent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // player‚ÌˆÚ“®‘¬“x
+    public float playerMoveSpeed = 0.0f;
+    public float playerTurnSpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
